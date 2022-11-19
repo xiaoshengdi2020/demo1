f54948b64c6a4b3f019d7b811db86fba5dc14961
Gtwbta <1026238441@qq.com>
2022-11-19 18:13:26 +0800

    11.19

---
+++ entry/src/main/java/com/example/demo1/MainAbility.java
@@ -0,0 +1,13 @@
+package com.example.demo1;
+
+import com.example.demo1.slice.MainAbilitySlice;
+import ohos.aafwk.ability.Ability;
+import ohos.aafwk.content.Intent;
+
+public class MainAbility extends Ability {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setMainRoute(MainAbilitySlice.class.getName());
+    }
+}
